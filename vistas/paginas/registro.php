d2b4121891a52195b38ccff4d3660adc1bdbb184
Brayan Andres <111899200+Brayan-Manco@users.noreply.github.com>
2022-11-05 01:37:40 -0500

    Diseño

--- vistas/paginas/registro.php
+++ vistas/paginas/registro.php
@@ -4,7 +4,7 @@
         <div class="circle-2"></div>
         <div class="circle-3"></div>
         <div class="box">
-            <h3>Bienvenido</h3>
+            <h3>Registro</h3>
             <form action="" method="post">
                 <div class="input-box">
                     <input type="text" class="input-control" id="nombre" name="registroNombre" placeholder="Nombre">
